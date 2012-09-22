4009fb1eef950d0d881a421bfda9ad51e2fc9d3a
Tom Robertshaw <me@tomrobertshaw.net>
2012-09-22 15:40:49 +0100

    Remove crap and inherit from the produc list block.

--- app/code/community/Meanbee/Mosaic/Block/Product/List/Mosaic.php
+++ app/code/community/Meanbee/Mosaic/Block/Product/List/Mosaic.php
@@ -1,91 +1,18 @@
 <?php
 
-class Meanbee_Mosaic_Block_Product_Mosaic extends Mage_Catalog_Block_Product_Abstract {
+class Meanbee_Mosaic_Block_Product_Mosaic extends Mage_Catalog_Block_Product_List {
 
-    protected $_productsCount = null;
-
-    protected $_defaultToolbarBlock = 'catalog/product_list_toolbar';
-
-    const DEFAULT_PRODUCTS_COUNT = '';
-
-    protected function _beforeToHtml()
+    protected function _getProductCollection()
     {
-        $toolbar = $this->getToolbarBlock();
+        if (is_null($this->_productCollection)) {
+            $collection = Mage::getResourceModel('catalog/product_collection');
+            Mage::getModel('catalog/layer')->prepareProductCollection($collection);
 
-        $todayDate  = Mage::app()->getLocale()->date()->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
 
-        $collection = Mage::getResourceModel('catalog/product_collection\');
-        Mage::getSingleton(\'catalog/product_status\')->addVisibleFilterToCollection($collection);
-        Mage::getSingleton(\'catalog/product_visibility\')->addVisibleInCatalogFilterToCollection($collection);
+            // your custom filter
 
-        $collection = $this->_addProductAttributesAndPrices($collection)
-            ->addStoreFilter()
-            ->addAttributeToFilter(\'special_price\', array(\'gt\' => 0), \'left\')
-            ->addAttributeToFilter(\'special_from_date\', array(\'date\' => true, \'to\' => $todayDate))
-            ->addAttributeToFilter(\'special_to_date\', array(\'or\'=> array(
-                0 => array(\'date\' => true, \'from\' => $todayDate),
-                1 => array(\'is\' => new Zend_Db_Expr(\'null\')))
-            ), \'left\')
-            ->addAttributeToSort(\'special_from_date\', \'desc\')
-            ->setPageSize($this->getProductsCount())
-            ->setCurPage(1)
-        ;
-        $this->setProductCollection($collection);
-
-         if ($orders = $this->getAvailableOrders()) {
-            $toolbar->setAvailableOrders($orders);
+            $this->_productCollection = $collection;
         }
-        if ($sort = $this->getSortBy()) {
-            $toolbar->setDefaultOrder($sort);
-        }
-        if ($modes = $this->getModes()) {
-            $toolbar->setModes($modes);
-        }
-
-        // set collection to tollbar and apply sort
-        $toolbar->setCollection($collection);
-
-        $this->setChild(\'toolbar\', $toolbar);
-        Mage::dispatchEvent(\'catalog_block_product_list_collection\', array(
-            \'collection\'=>$collection,
-        ));
-
-        return parent::_beforeToHtml();
-    }
-
-    public function getToolbarBlock()
-    {
-        if ($blockName = $this->getToolbarBlockName()) {
-            if ($block = $this->getLayout()->getBlock($blockName)) {
-                return $block;
-            }
-        }
-        $block = $this->getLayout()->createBlock($this->_defaultToolbarBlock, microtime());
-        return $block;
-    }
-
-    public function setProductsCount($count)
-    {
-        $this->_productsCount = $count;
-        return $this;
-    }
-
-    public function getProductsCount()
-    {
-        if (null === $this->_productsCount) {
-            $this->_productsCount = self::DEFAULT_PRODUCTS_COUNT;
-        }
-        return $this->_productsCount;
-    }
-
-
-     public function getMode()
-    {
-        return $this->getChild(\'toolbar\')->getCurrentMode();
-    }
-
-    public function getToolbarHtml()
-    {
-        return $this->getChildHtml(\'toolbar\');
+        return $this->_productCollection;
     }
 }

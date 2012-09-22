4803197a9b80509c787e64c25fa3d624589d9b19
Tom Robertshaw <me@tomrobertshaw.net>
2012-09-22 16:07:37 +0100

    Rename block and add main product image to collection.

--- app/code/community/Meanbee/Mosaic/Block/Product/List/Mosaic.php
+++ app/code/community/Meanbee/Mosaic/Block/Product/List/Mosaic.php
@@ -1,11 +1,12 @@
 <?php
 
-class Meanbee_Mosaic_Block_Product_Mosaic extends Mage_Catalog_Block_Product_List {
+class Meanbee_Mosaic_Block_Product_List_Mosaic extends Mage_Catalog_Block_Product_List {
 
     protected function _getProductCollection()
     {
         if (is_null($this->_productCollection)) {
             $collection = Mage::getResourceModel('catalog/product_collection');
+            $collection->addAttributeToSelect("image");
             Mage::getModel('catalog/layer')->prepareProductCollection($collection);
 
 

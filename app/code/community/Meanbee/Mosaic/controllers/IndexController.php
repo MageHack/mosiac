8cc969705db51bd069fd694ae2ad9d2315006cf6
Martin Shaw <martin.shaw@meanbee.com>
2012-09-22 12:41:21 +0100

    Update the layout to empty

--- app/code/community/Meanbee/Mosaic/controllers/IndexController.php
+++ app/code/community/Meanbee/Mosaic/controllers/IndexController.php
@@ -1,7 +1,8 @@
 <?php
 class Meanbee_Mosaic_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction() {
-        echo "HEY";
+        $this->loadLayout();
+        $this->renderLayout();
     }
 
 }

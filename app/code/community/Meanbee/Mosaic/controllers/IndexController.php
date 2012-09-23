4c411b3d0f6d7126cb276e304825a2c2225cccd5
Tom Robertshaw <me@tomrobertshaw.net>
2012-09-23 12:34:59 +0100

    Handle product collection better.

--- app/code/community/Meanbee/Mosaic/controllers/IndexController.php
+++ app/code/community/Meanbee/Mosaic/controllers/IndexController.php
@@ -1,8 +1,96 @@
 <?php
-class Meanbee_Mosaic_IndexController extends Mage_Core_Controller_Front_Action {
+require_once 'Mage/Catalog/controllers/CategoryController.php';
+
+class Meanbee_Mosaic_IndexController extends Mage_Catalog_CategoryController {
+
+    /**
+     * Initialize requested category object
+     *
+     * @return Mage_Catalog_Model_Category
+     */
+    protected function _initCatagory() {
+        Mage::dispatchEvent('catalog_controller_category_init_before', array('controller_action' => $this));
+        $categoryId = Mage::app()->getStore()->getRootCategoryId();
+
+        if (!$categoryId) {
+            return false;
+        }
+
+        $category = Mage::getModel('catalog/category')
+            ->setStoreId(Mage::app()->getStore()->getId())
+            ->load($categoryId);
+
+
+        Mage::getSingleton('catalog/session')->setLastVisitedCategoryId($category->getId());
+        Mage::register('current_category', $category);
+
+        try {
+            Mage::dispatchEvent(
+                'catalog_controller_category_init_after',
+                array(
+                    'category' => $category,
+                    'controller_action' => $this
+                )
+            );
+        } catch (Mage_Core_Exception $e) {
+            Mage::logException($e);
+            return false;
+        }
+
+        return $category;
+    }
+
+
     public function indexAction() {
-        $this->loadLayout();
-        $this->renderLayout();
+        $this->_forward("view");
+    }
+
+    public function viewAction() {
+        if ($category = $this->_initCatagory()) {
+            $design = Mage::getSingleton('catalog/design');
+            $settings = $design->getDesignSettings($category);
+
+            // apply custom design
+            if ($settings->getCustomDesign()) {
+                $design->applyCustomDesign($settings->getCustomDesign());
+            }
+
+            Mage::getSingleton('catalog/session')->setLastViewedCategoryId($category->getId());
+
+            $update = $this->getLayout()->getUpdate();
+            $update->addHandle('default');
+
+            $this->addActionLayoutHandles();
+            $update->addHandle('CATEGORY_' . $category->getId());
+            $this->loadLayoutUpdates();
+
+            // apply custom layout update once layout is loaded
+            if ($layoutUpdates = $settings->getLayoutUpdates()) {
+                if (is_array($layoutUpdates)) {
+                    foreach($layoutUpdates as $layoutUpdate) {
+                        $update->addUpdate($layoutUpdate);
+                    }
+                }
+            }
+
+            $this->generateLayoutXml()->generateLayoutBlocks();
+            // apply custom layout (page) template once the blocks are generated
+            if ($settings->getPageLayout()) {
+                $this->getLayout()->helper('page/layout')->applyTemplate($settings->getPageLayout());
+            }
+
+            if ($root = $this->getLayout()->getBlock('root')) {
+                $root->addBodyClass('categorypath-' . $category->getUrlPath())
+                    ->addBodyClass('category-' . $category->getUrlKey());
+            }
+
+            $this->_initLayoutMessages('catalog/session');
+            $this->_initLayoutMessages('checkout/session');
+            $this->renderLayout();
+        }
+        elseif (!$this->getResponse()->isRedirect()) {
+            $this->_forward('noRoute');
+        }
     }
 
 }

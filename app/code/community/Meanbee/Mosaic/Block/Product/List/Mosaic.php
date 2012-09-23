caec11b218006b778abdebf2eec4f89517d83027
Tom Robertshaw <me@tomrobertshaw.net>
2012-09-23 14:54:48 +0100

    Adjust page size for slightly better demos.

--- app/code/community/Meanbee/Mosaic/Block/Product/List/Mosaic.php
+++ app/code/community/Meanbee/Mosaic/Block/Product/List/Mosaic.php
@@ -7,7 +7,7 @@
 class Meanbee_Mosaic_Block_Product_List_Mosaic extends Mage_Catalog_Block_Product_List {
 
     protected $_bestselling_groups = 3;
-    const PAGE_SIZE = 30;
+    const PAGE_SIZE = 20;
 
     protected function _getProductCollection()
     {
